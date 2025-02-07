28f3d63f0f0b3d1122b9c61b6133b69a949e396c
Lars Nesheim <l.nesheim@ucl.ac.uk>
2025-02-07 17:42:33 +0000

    Create load_intel.sh

---
+++ load_intel.sh
@@ -0,0 +1,6 @@
+module load intel/compiler-rt intel/ifort
+module load intel/tbb intel/mkl
+module load intel/mpi
+source /opt/NAG/nll6i302bl/scripts/nagvars.sh int32 static vendor
+export NAG_KUSARI_FILE=/opt/NAG/licences/licence.dat
+#module load nag
